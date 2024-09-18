1ce276479f34963c9c057abccdf8dd170e602ed1
spoon122 <spoon122@DESKTOP-V8S8IL9>
2024-09-18 19:44:01 +0300

    fixing git issues

--- IdentityApi/Extensions.cs
+++ IdentityApi/Extensions.cs
@@ -1,7 +1,42 @@
-﻿namespace IdentityApi
+﻿using IdentityApi.DataAccess.Models;
+using IdentityApi.DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace IdentityApi
 {
     public static class Extensions
     {
+        public static IServiceCollection AddIdentityDb(this IServiceCollection services, WebApplication builder)
+        {
+            services.AddIdentity<User, IdentityRole>()
+                    .AddEntityFrameworkStores<UserDbContext>()
+                    .AddDefaultTokenProviders();
 
+            services.AddDbContext<UserDbContext>(options =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString(nameof(UserDbContext));
+                options.UseNpgsql(builder.Configuration.GetConnectionString(connectionString));
+            });
+
+            return services;
+                                 
+        }
+
+        public static IServiceCollection AddCookieConfig(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.SlidingExpiration = true;
+            });
+            services.Configure<DataProtectionTokenProviderOptions>(opt =>
+                               opt.TokenLifespan = TimeSpan.FromMinutes(15));
+
+            return services;
+        }
     }
 }
--- IdentityApi/Program.cs
+++ IdentityApi/Program.cs
@@ -1,39 +1,21 @@
-using IdentityApi.DataAccess;
-using IdentityApi.DataAccess.Models;
 using IdentityApi.Endpoints;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromDays(30);
-    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
-    options.SlidingExpiration = true;
-});
 
 builder.Services.AddAuthorization();
 
 builder.AddServiceDefaults();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddIdentity<User, IdentityRole>()
-        .AddEntityFrameworkStores<UserDbContext>()
-        .AddDefaultTokenProviders();
-
-builder.Services.Configure<DataProtectionTokenProviderOptions>(opt =>
-        opt.TokenLifespan = TimeSpan.FromMinutes(15));
-
-builder.Services.AddDbContext<UserDbContext>(options => 
-        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(UserDbContext))));
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthentication();
 app.UseAuthorization();
 
